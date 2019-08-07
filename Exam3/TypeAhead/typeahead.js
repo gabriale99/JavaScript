627437734b477f330024add2783fc28d244c54d7
gabriale99 <gabrielchung1216@gmail.com>
2019-08-06 19:17:16 -0700

    question 3 completed

--- Exam3/TypeAhead/typeahead.js
+++ Exam3/TypeAhead/typeahead.js
@@ -1,20 +1,24 @@
 'use strict'
-const arr = ['darth', 'jar jar bink', 'yoda', 'Anakin SkyWalker', 'Luke SkyWalker', 'princess leia']
 
 function removeDropdown(container) {
   container.removeChild(container.lastChild);
 }
 
 function typeAheadView(container, model) {
-  function render(data = document.createElement('ul'), state) {
+  function render(data = document.createElement('ul'), character = null) {
     let dropdownMenu = container.querySelector('.dropdown-container');
     removeDropdown(dropdownMenu);
     dropdownMenu.appendChild(data);
-    // console.log(state)
-    if (!state) return;
-    else {
-      let choseState = document.querySelector('.character');
-      choseState.innerHTML = state;
+
+    let info = container.querySelector('.character');
+    if (!!character) {
+      info.innerHTML = '';
+      for (let prop in character) {
+        let div = document.createElement('div');
+        div.className = 'character-prop'
+        div.innerHTML = `${prop}: ${character[prop]}`;
+        info.appendChild(div);
+      }
     }
   }
 
@@ -24,49 +28,60 @@ function typeAheadView(container, model) {
 }
 
 function typeAheadModel() {
-  let data, state;
+  let data = document.createElement('ul');
+  data.className = "dropdown";
+
+  let character;
   let form = document.querySelector('form');
   let input = form.querySelector('input[type="textarea"]')
   let subscriber;
 
   function updateDropdown() {
-    data = document.createElement('ul');
-    data.className = "dropdown";
     let cur = input.value;
-    state = '';
-    if (!!cur) {
+    character = '';
+    if (cur !== "") {
       let regex = new RegExp(`(.)*${cur}(.)*`, "gi")
-      for (let item of arr) {
-        if (!item.match(regex)) {
-          continue;
-        }
-        let option = document.createElement('li');
-        option.innerHTML = item;
-        data.appendChild(option);
-      }
-
-      if (data.children.length === 1) {
-        state = data.firstChild.innerHTML;
-      }
-
-      data.addEventListener('click', function () {
-        if (event.target.closest('li')) {
-          input.value = event.target.textContent;
+      fetch('https://swapi.co/api/people/')
+        .then(response => response.json())
+        .then(json => {
           data.innerHTML = '';
-          state = input.value;
-          input.focus();
-        }
-      });
+          json.results.forEach(result => {
+            if (result['name'].match(regex)) {
+              let option = document.createElement('li');
+              option.innerHTML = result['name'];
+              data.appendChild(option);
+            }
+          })
+        });
     }
+
+    if (data.children.length === 1) {
+      character = data.firstChild.innerHTML;
+    }
+
+    data.addEventListener('click', function () {
+      input.value = event.target.textContent;
+      data.innerHTML = '';
+      character = input.value;
+      console.log("click: " + character);
+      input.focus();
+    });
+
     subscriber(data);
   }
 
-  input.addEventListener('input', updateDropdown);
+  input.addEventListener('input', () => {
+    updateDropdown();
+  });
 
   form.onsubmit = function () {
     data.innerHTML = input.value = '';
-    console.log(state)
-    subscriber(data, state);
+    fetch(`https://swapi.co/api/people/?search=${character}`)
+      .then(response => response.json())
+      .then(json => {
+        // console.log(json)
+        subscriber(data, json.results[0]);
+      })
     return false;
   }
 
